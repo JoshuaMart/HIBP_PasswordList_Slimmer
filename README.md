3bd3fba54d6cc79a7a708ff450a82d579a8dbbd6
Jomar <contact@jomar.fr>
2020-04-30 12:58:55 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # HIBP_PasswordList_Slimmer
-Converted the text list to binary to save space
+![Banner](https://zupimages.net/up/20/18/xfr4.png)![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg) ![made-with-go](https://img.shields.io/badge/malde%20with-go-blue)  ![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)
+
+Tool in Go designed to reduce the size of Have I Been Pwned password lists and speed up searching.
+
+## Usage
+Clone the repo or download one of the binaries in the releases.
+
+```bash
+go run .\PwnDBConverter.go {InputHashFile} {HashType} {OutputFile}
+.\PwnDBConverter.exe {InputHashFile} {HashType} {OutputFile}
+```
+![Usage](https://zupimages.net/up/20/18/op6u.png)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func main() {
+	hashFile, hashType, outputFile := flagCheck()
+
+	// Starting the time counter
+	startTimer := time.Now()
+
+	// OPEN INPUT FILE
+	file, _ := os.Open(hashFile)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// OPEN OUTPUT FILE
+	output, _ := os.OpenFile(outputFile, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	defer file.Close()
+
+	// HASH TYPE
+	var hashLength int
+	if hashType == "NTLM" {
+		hashLength = 32
+	} else if hashType == "SHA1" {
+		hashLength = 40
+	}
+
+	for scanner.Scan() {
+		hash := scanner.Text()[:hashLength]
+		occurence := scanner.Text()[33:]
+
+		hash_byte, _ := hex.DecodeString(hash)
+		output.Write(hash_byte)
+
+		occurence_to_int, _ := strconv.Atoi(occurence)
+		b := make([]byte, 4)
+		binary.BigEndian.PutUint32(b, uint32(occurence_to_int))
+		output.Write(b)
+	}
+
+	log.Printf("It took : %s", time.Since(startTimer))
+}
+
+// Check if flag is provided
+func flagCheck() (string, string, string) {
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 3 {
+		fmt.Println("Input and/or hash type and/or output file is missing.")
+		os.Exit(1)
+	}
+	return args[0], args[1], args[2]
+}
